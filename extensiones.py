5d3ae7a9c7852cf4cee9f585021a43b8b60e20f5
Cristian Irribarra <cristian.irribarra@cenia.cl>
2024-09-01 17:09:54 -0400

    added extensiones.py with some git indications

---
+++ extensiones.py
@@ -0,0 +1,16 @@
+# EXTENSIONES RECOMENDADAS
+#Copilot
+#algun color theme
+#git graph
+#git history
+#jupyter (todas las que pillen)
+#rainbow csv
+
+"""
+git crash course
+
+git add (archivo) -> agrega un archivo al stage	
+git commit -m "mensaje" -> guarda los cambios en el repositorio
+git push origin master -> sube los cambios al repositorio
+git pull origin master -> baja los cambios del repositorio
+"""
